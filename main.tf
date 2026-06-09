1ed89d55517a27c224c4b0476968563ee565cddc
pvlasak <petr.vlasakk@gmail.com>
2026-06-09 13:46:56 +0200

    initial commit after creating vpc and subnet

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable env_prefix {}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-subnet"
+    }
+}
