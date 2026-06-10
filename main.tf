a5825481e8da730e4ee4eb2aaab89d419b9c755d
pvlasak <petr.vlasakk@gmail.com>
2026-06-10 09:04:20 +0200

    user_data script is added to instance to be executed after instance is created.

--- main.tf
+++ main.tf
@@ -107,6 +107,16 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
+    user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y && sudo yum install docker -y
+                sudo systemctl start docker
+                sudo usermod -aG docker ec2-user
+                docker run -p 8080:80 nginx
+            EOF
+    
+    user_data_replace_on_change = true
+
     tags = {
         Name: "${var.env_prefix}-server"
     }
