21d3a4ad0fa8631c7ca2e0bee96ed6d7cfa9b0bb
pvlasak <petr.vlasakk@gmail.com>
2026-06-11 16:07:22 +0200

    default route table edited instead of creating a new one

--- main.tf
+++ main.tf
@@ -33,20 +33,15 @@ resource "aws_internet_gateway" "myapp-igw" {
     }    
 }
 
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id 
+resource "aws_default_route_table" "main-route-table" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id 
     }
     tags = {
-        Name: "${var.env_prefix}-rtb"
+        Name: "${var.env_prefix}-main-rtb"
     }
-}
-
-resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id 
-    route_table_id = aws_route_table.myapp-route-table.id 
 }
 
 resource "aws_security_group" "my-app-sg" {
