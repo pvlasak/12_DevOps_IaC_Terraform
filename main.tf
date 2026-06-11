378946b700af10a7b88facf578c9a52da05f4723
pvlasak <petr.vlasakk@gmail.com>
2026-06-11 16:54:13 +0200

    user_data moved to entry_script.sh

--- main.tf
+++ main.tf
@@ -101,13 +101,7 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = <<EOF
-                #!/bin/bash
-                sudo yum update -y && sudo yum install docker -y
-                sudo systemctl start docker
-                sudo usermod -aG docker ec2-user
-                docker run -p 8080:80 nginx
-            EOF
+    user_data = file("entry_script.sh")
     
     user_data_replace_on_change = true
 
