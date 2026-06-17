7bece2a659010626ff18f87018363a6ed5ba70a7
pvlasak <petr.vlasakk@gmail.com>
2026-06-17 15:56:38 +0200

    variable declaration removed from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-variable "region" {
-    default = "eu-central-1"
-}
-
-variable env_prefix {
-    default = "dev"
-}
-variable vpc_cidr_block {
-    default = "10.0.0.0/16"
-}
-variable subnet_cidr_block {
-    default = "10.0.10.0/24"
-}
-variable avail_zone {
-    default = "eu-central-1a"
-}
-variable my-ip {
-    default = "212.11.106.250"
-}
-variable jenkins_ip {
-    default = "64.226.125.108/32"
-}
-variable instance_type {
-    default = "t2.micro"
-}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
